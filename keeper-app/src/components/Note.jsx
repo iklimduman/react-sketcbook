9093d399c5d3b62d433d37efe66d0df82da30693
Iklim Duman <76881313+iklimduman@users.noreply.github.com>
2021-06-08 10:54:51 +0300

    Update Note.jsx

--- keeper-app/src/components/Note.jsx
+++ keeper-app/src/components/Note.jsx
@@ -1,1 +1,10 @@
+import react from "react";
 
+function Note(){
+    return <div className="note">
+        <h1 className="h1">Note title</h1>
+        <p className="p">Note text</p>
+    </div>
+};
+
+export default Note;
