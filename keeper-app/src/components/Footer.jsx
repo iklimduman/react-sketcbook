827f1daa80606d7bac0e846b6b3821d6d54a4c5d
Iklim Duman <76881313+iklimduman@users.noreply.github.com>
2021-06-08 10:54:14 +0300

    Update Footer.jsx

--- keeper-app/src/components/Footer.jsx
+++ keeper-app/src/components/Footer.jsx
@@ -1,1 +1,14 @@
+import React from "react";
 
+var date = new Date();
+var dateTime = date.getDay() + "/" + date.getMonth() + "/" + date.getFullYear();
+
+function footer(){
+    return <footer>
+        <p className="p">Copyright
+        <br/>
+        {dateTime}</p>
+        </footer>
+};
+
+export default footer ;
