6cfccb15cb59903941e20707dc79ed323588ff5f
Iklim Duman <76881313+iklimduman@users.noreply.github.com>
2021-06-10 10:57:09 +0300

    add default notes

---
+++ keeper-app/src/notes.js
@@ -0,0 +1,17 @@
+const notes = [
+    {
+      key: 1,
+      title: "Drink more water!",
+      content:
+        "Drinking water can prevent dehydration, a condition that can cause unclear thinking, result in mood change, cause your body to overheat, and lead to constipation and kidney stones."
+    },
+    {
+      key: 2,
+      title: "Eat healthy!",
+      content:
+        "It protects you against many chronic noncommunicable diseases, such as heart disease, diabetes and cancer."
+    }
+  ];
+  
+  export default notes;
+  
