a48377ffbf4c9e77709c5a1dc2c0090a5e27df59
Iklim Duman <76881313+iklimduman@users.noreply.github.com>
2021-06-10 10:58:24 +0300

    react props

--- keeper-app/src/components/Note.jsx
+++ keeper-app/src/components/Note.jsx
@@ -2,8 +2,8 @@ import react from "react";
 
 function Note(){
     return <div className="note">
-        <h1 className="h1">Note title</h1>
-        <p className="p">Note text</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
 };
 
