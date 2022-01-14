f4f17032444156b292ade8ee232f3266820b7fda
Iklim Duman <76881313+iklimduman@users.noreply.github.com>
2022-01-14 17:54:18 +0300

    Create index.js

---
+++ ReactHooks/useLayoutEffect/index.js
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+
+import Model from "./Model";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <Model />,
+    rootElement
+);
